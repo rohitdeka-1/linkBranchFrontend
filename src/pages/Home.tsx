fd779fa5ef76c84dee804b4d3b4848b23393b0ca
Rohit Deka <studyrhd@gmail.com>
2025-04-18 11:53:36 +0530

    "Button animated and Footer"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import Logo from "./components/Logo";
 import { Link } from "react-router-dom";
+import Footer from "./components/Footer";
 const Home = () => {
   return (
     <motion.div
@@ -19,7 +20,7 @@ const Home = () => {
       }}
     >
       <Logo />
-      <div className="min-h-screen flex flex-col justify-start items-start relative pb-40 text-white p-4 space-y-8 ">
+      <div className="min-h-screen flex flex-col justify-start items-start relative  text-white p-4 space-y-8 ">
         <div className="flex flex-col space-y-4 mt-20">
           <h2 className="font-extralight p-4 text-3xl w-9/12 leading-relaxed">
             Multiple Links for Your Link in Bio
@@ -50,19 +51,36 @@ const Home = () => {
           <img src="./coverPhoto.png"></img>
         </div>
 
-        <div className="flex  justify-center items-center flex-col px-4 w-full ">
-          <h1 className="text-3xl font-bold">Features</h1>
-          <p className="text-slate-300 py-6 text-justify">
-            LinkBranch is a powerful tool that allows you to create a
-            personalized and customizable page that houses all your important links.
-            Whether you're a content creator, business owner, or just someone who
-            wants to share their favorite links, LinkBranch has got you covered.
-          </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 w-full">
+          <div className="flex flex-col justify-center items-start">
+            <h1 className="text-3xl font-bold w-full text-center">Features</h1>
+            <p className="text-slate-300 py-6 text-justify">
+              LinkBranch is a powerful tool that allows you to create a
+              personalized and customizable page that houses all your important links.
+              Whether you're a content creator, business owner, or just someone who
+              wants to share their favorite links, LinkBranch has got you covered.
+            </p>
           </div>
 
-        <div className="flex flex-row px-4 space-x-7 w-full mt-10">
+         
+
+          <div className="flex flex-col justify-center items-start">
+            <h1 className="text-3xl font-bold  w-full text-center">Pricing</h1>
+            <p className="text-slate-300 py-6 text-justify">
+              LinkBranch offers flexible pricing plans to suit your needs. From a free
+              plan with basic features to premium plans with advanced analytics and
+              customization options, there's something for everyone.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row px-4 w-full ">
           <img src="./cover2.png"></img>
         </div>
+       <div className="w-full">
+
+       <Footer/>
+       </div>
+        
       </div>
     </motion.div>
   );
--- src/pages/components/Logo.tsx
+++ src/pages/components/Logo.tsx
@@ -4,11 +4,19 @@ const Logo = () => {
   return (
     <div className="w-full fixed z-[100] backdrop-blur-sm top-0 h-14 left-0  flex justify-between px-6 xl:px-40 items-center border-b py-2 border-gray-500 ">
       <div className="w-full flex  items-center space-x-2">
-        <img src="./logo1.png" className=" h-5 "/>
-        <h1 className="text-white text-xl font-bold"><Link to="/">LinkBranch</Link></h1>
+        <img src="./logo1.png" className=" h-5 " />
+        <h1 className="text-white text-xl font-bold">
+          <Link to="/">LinkBranch</Link>
+        </h1>
       </div>
-      <div className=" flex  items-center space-x-2 text-white">
-        <a href="https://www.rohitdeka.me" target="_blank">Developer</a>
+      <div className=" flex justify-center w-1/3  items-center space-x-2 text-white">
+        <a
+          href="https://www.rohitdeka.me"
+          target="_blank"
+          className="relative inline-block px-4 py-2 text-white font-medium rounded-md overflow-hidden group"
+        > <span className="absolute inset-0 bg-gradient-to-r from-slate-500 via-blue-500 to-purple-500 opacity-0 group-hover:opacity-100 blur-sm animate-gradient-x transition-opacity duration-500"></span>
+        <span className="relative z-10">Developer</span>
+      </a>
       </div>
     </div>
   );
