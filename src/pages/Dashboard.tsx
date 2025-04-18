b297f05016a5e928e9f3331b47a84b258f3a8961
Rohit Deka <studyrhd@gmail.com>
2025-04-18 23:11:56 +0530

    dash

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -23,7 +23,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchMe = async () => {
       try {
-        const res = await axios.get("/api/v1/user/me", {
+        const res = await axios.get(`${import.meta.env.VITE_BACKEND_URI}/api/v1/user/me`, {
           withCredentials: true, 
         });
 
